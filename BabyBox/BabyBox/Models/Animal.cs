8a56468b4399c1c64a5c01710f19a2e34ba4d9f1
Hiddebro <hiddebr9@gmail.com>
2021-12-06 13:59:18 +0100

    Hidde
    Casses added
    - User
    - SimonSays
    - Temperature
    - Light
    - Animal

---
+++ BabyBox/BabyBox/Models/Animal.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BabyBox.Models
+{
+    public class Animal
+    {
+        public bool OnSwitch { get; set; }
+        public Array Sounds { get; set; }
+
+        public Animal(bool onswitch, Array sounds)
+        {
+            OnSwitch = onswitch;
+            Sounds = sounds;
+
+        }
+        public Animal()
+        {
+
+        }
+    }
+}
---
+++ BabyBox/BabyBox/Models/SimonSays.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BabyBox.Models
+{
+    public class SimonSays
+    {
+        public int Time { get; set; }
+        public int SetTime { get; set; }
+
+        public SimonSays(int time, int settime)
+        {
+            Time = time;
+            SetTime = settime;
+
+        }
+        public SimonSays()
+        {
+
+        }
+    }
+}
